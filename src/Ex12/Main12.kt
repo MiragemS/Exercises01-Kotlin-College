34f32c3eceb251111e7b5dc190f89a61b4e6ff05
MURILO SANTOS <murilo.santos54@fatec.sp.gov.br>
2025-03-10 12:45:18 -0300

    Modulo de exercicios pt. 2.1

--- src/Ex12/Main12.kt
+++ src/Ex12/Main12.kt
@@ -1,5 +1,23 @@
 package Ex12
 
 fun main() {
-    
+    val PESQUISADOS = 10
+    var salario = 0.0
+    var maiorSalario = 0
+    var numeroFilhos = 0
+
+    println("---------- Pesquisa Cidadã ----------")
+
+    for(i in 1..PESQUISADOS){
+        println("Insira seu salário: ")
+        salario += readln().toDouble()
+        println("Insira quantos filhos tem: ")
+        numeroFilhos += readln().toInt()
+        println("-------------------\n")
+    }
+
+    println("---------- Resultados da pesquisa ----------")
+    println("Maior salário: ${maiorSalario} \n" +
+            "Média Salarial: ${salario/PESQUISADOS} \n" +
+            "Média de filhos: ${numeroFilhos/PESQUISADOS}")
 }
